7f096a20bb96c4d823d23e4d42bff503e4ca64db
JosephOkumu <jokumu25@gmail.com>
2024-06-10 05:10:24 +0300

    adding alternative code as comments.

--- main.go
+++ main.go
@@ -11,6 +11,29 @@ func compare(a, b string) int {
 		return 0
 	}
 
+
+	// This code below should also work the same way.
+	// runesA := []rune(a)
+	// runesB := []rune(b)
+	// i := 0
+	// j := 0
+
+	// for i < len(runesA) && j < len(runesB) {
+	// 	if runesA[i] < runesB[j] {
+	// 		return -1
+	// 	}else if runesA[i] > runesB[j] {
+	// 		return 1
+	// 	}
+	// 	i++
+	// 	j++
+	// }	
+	// if len(runesA) < len(runesB) {
+	// 	return -1
+	// }else if len(runesA) > len(runesB){
+	// 	return 1
+	// }
+	// return 0
+
 }
 
 func main() {
